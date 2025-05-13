15e162760e674eb0812d5823470e4f81f9975f6d
leejaejeong <jaejeonglee6@gmail.com>
2025-05-13 09:55:35 +0900

    수정

--- src/component/Section01.js
+++ src/component/Section01.js
@@ -7,17 +7,20 @@ function Section01() {
     useEffect(() => {
 
         function setvhSize() {
-            let vh = window.innerHeight * 0.01;
+            // let vh = window.innerHeight * 0.01;
+            let vh = window.VisualViewport.height * 0.01;
             document.documentElement.style.setProperty('--vh', `${vh}px`);
         }
 
         setvhSize();
 
         window.addEventListener('resize', setvhSize);
+        window.VisualViewport?.addEventListener('resize', setvhSize);
 
         // cleanup 함수로 리스너 제거
         return () => {
             window.removeEventListener('resize', setvhSize);
+            window.VisualViewport?.removeEventListener('resize', setvhSize);
         };
 
     }, []);
